8cbcf747b6e48bf234453e9470b2db3312ebdcf9
ChrisKnowsDev <chrisharley81@gmail.com>
2021-01-03 15:56:10 -0500

    init

---
+++ src/list.js
@@ -0,0 +1,20 @@
+const list = [
+  {
+    title: 'react',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0,
+  },
+  {
+    title: 'redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1,
+  },
+]
+
+export default list
